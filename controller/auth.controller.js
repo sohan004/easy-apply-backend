5a9b797044bad201455a1165bc27732af5ad2f59
sohan004 <aryansohan02@gmail.com>
2025-06-02 23:19:53 +0600

    NEW CHANGES: 2025-06-02 23:19:53

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -135,11 +135,9 @@ module.exports.getInfo = asyncErrorCatcher(async (req, res) => {
   let user = await User.findById(id).select("-password -refreshToken");
   const totalTemplate = await Template.countDocuments({ user: id });
   const totalMailSubmitted = await Mail.countDocuments({ user: id });
-  const host =  req?.get("host");
-  const protocol =  req?.protocol;
-  user["profilePicture"] = `${protocol}://${host}${user.profilePicture}`;
-  user["totalTemplate"] = totalTemplate;
-  user["totalMailSubmitted"] = totalMailSubmitted;
+  const host = await req?.get("host");
+  const protocol = await req?.protocol;
+  user["profilePicture"] =  getFileFullUrl(req, user?.profilePicture);
   res.json({ user, success: true });
 });
 
